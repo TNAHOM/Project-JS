b9c242f535e50516bc922ef504e26d68b20c5e62
Nahom <nahomt2419@gmail.com>
2024-07-29 15:21:49 +0300

    Used an input field rather than prompt

--- TODO-app/todo.js
+++ TODO-app/todo.js
@@ -5,7 +5,7 @@ let count = 1
 
 let addTodo = (task) => {
     let addelement = document.createElement('li')
-    let text = document.createTextNode("Id Number: " + count + ' ' + task);
+    let text = document.createTextNode("ID: " + count + ' ' + task);
     addelement.appendChild(text)
     
     let under = document.getElementById('ele');
@@ -13,36 +13,46 @@ let addTodo = (task) => {
     
     addelement.setAttribute('id', count)
     count = count + 1
+    document.getElementById('text').value = "";
+
 }
 
 let removeFunc = (enterId) => {
     getId = document.getElementById(enterId)
-
+    console.log(getId, enterId)
     if (!getId) {
-        alert('The Id you enterd is invalid, please check again')
+        alert('The Id you enterd is invalid or No ID have been given  please check again')
     } else {
         getId.remove()
+        document.getElementById('id').value = "";
     }
 }
 
 let editFunc = (enterId, editedText) => {
     getId = document.getElementById(enterId)
-    getId.innerHTML = "Id Number: " + enterId +" " + editedText
+    getId.innerHTML = "NO: " + enterId +" " + editedText
+    document.getElementById('id').value = "";
+    document.getElementById('text').value = "";
+
 }
 
 
 add.onclick = () => {
-    const task = prompt('Add a task', );
-    addTodo(task)
+    // const task = prompt('Add a task', );
+    let inputText= document.getElementById('text')
+    addTodo(inputText.value)
 }
 
 remove.onclick = () => {
-    const enterId = prompt('Enter Id you want t delete: ')
-    removeFunc(enterId)
+    // const enterId = prompt('Enter Id you want t delete: ')
+    let inputId = document.getElementById('id')
+    removeFunc(inputId.value)
 }
 
 edit.onclick = () => {
-    const getId = prompt('Enter the id you want to edit: ')
-    const editedText = prompt('')
-    editFunc(getId, editedText)
+    // const getId = prompt('Enter the id you want to edit: ')
+    // const editedText = prompt('')
+    let inputText = document.getElementById('text')
+    let inputId = document.getElementById('id')
+    editFunc(inputId.value, inputText.value)
 }
